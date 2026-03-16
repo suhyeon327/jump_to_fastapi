c362a0e8695bf044319b063d9ea8d628f2d54437
Jang SuHyeon <jshh020327@gmail.com>
2026-03-17 00:46:15 +0900

    feat: implement user registration feature

---
+++ frontend/src/lib/store.js
@@ -0,0 +1,11 @@
+import { writable } from 'svelte/store'
+
+const  persisit_storage = (key, initValue) => {
+    const storedValueStr = localStorage.getItem(key)
+    const store = writable(storedValueStr != null ? JSON.parse(storedValueStr) : initValue)
+    store.subscribe((val) => {
+        localStorage.setItem(key, JSON.stringify(val))
+    })
+    return store
+}
+export const page = persisit_storage("page", 0)
